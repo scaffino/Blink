4cb02adb7739301694efd9168452ee09652bbc05
Giulia Scaffino <giulia.scaffino@gmail.com>
2024-09-09 09:50:36 +0200

    pep8 compliance

--- src/schemas.py
+++ src/schemas.py
@@ -2,233 +2,123 @@ schema_block_header = {
     "type": "object",
     "properties": {
         "result": {
-        "type": "object",
-        "properties": {
-            "hash": {
-            "type": "string"
+            "type": "object",
+            "properties": {
+                "hash": {"type": "string"},
+                "confirmations": {"type": "integer"},
+                "height": {"type": "integer"},
+                "version": {"type": "integer"},
+                "versionHex": {"type": "string"},
+                "merkleroot": {"type": "string"},
+                "time": {"type": "integer"},
+                "mediantime": {"type": "integer"},
+                "nonce": {"type": "integer"},
+                "bits": {"type": "string"},
+                "difficulty": {"type": "number"},
+                "chainwork": {"type": "string"},
+                "nTx": {"type": "integer"},
+                "previousblockhash": {"type": "string"},
+                "nextblockhash": {"type": "string"}
             },
-            "confirmations": {
-            "type": "integer"
-            },
-            "height": {
-            "type": "integer"
-            },
-            "version": {
-            "type": "integer"
-            },
-            "versionHex": {
-            "type": "string"
-            },
-            "merkleroot": {
-            "type": "string"
-            },
-            "time": {
-            "type": "integer"
-            },
-            "mediantime": {
-            "type": "integer"
-            },
-            "nonce": {
-            "type": "integer"
-            },
-            "bits": {
-            "type": "string"
-            },
-            "difficulty": {
-            "type": "number"
-            },
-            "chainwork": {
-            "type": "string"
-            },
-            "nTx": {
-            "type": "integer"
-            },
-            "previousblockhash": {
-            "type": "string"
-            },
-            "nextblockhash": {
-            "type": "string"
-            }
+            "required": [
+                "hash",
+                "confirmations",
+                "height",
+                "version",
+                "versionHex",
+                "merkleroot",
+                "time",
+                "mediantime",
+                "nonce",
+                "bits",
+                "difficulty",
+                "chainwork",
+                "nTx",
+                "previousblockhash"
+            ]
         },
-        "required": [
-            "hash", 
-            "confirmations",
-            "height",
-            "version", 
-            "versionHex",
-            "merkleroot",
-            "time", 
-            "mediantime",
-            "nonce",
-            "bits",
-            "difficulty",
-            "chainwork", 
-            "nTx", 
-            "previousblockhash"
-        ]
-        },
-        "error": {
-        "type": ["string", "null"]
-        },
-        "id": {
-        "type": ["string", "null"]
-        }
+        "error": {"type": ["string", "null"]},
+        "id": {"type": ["string", "null"]}
     },
-    "required": [
-        "result",
-        "error",
-        "id"
-    ]
-    }
+    "required": ["result", "error", "id"]
+}
 
 schema_proof = {
-  "type": "object",
-  "properties": {
-    "result": {
-      "type": "string"
+    "type": "object",
+    "properties": {
+        "result": {"type": "string"},
+        "error": {"type": ["string", "null"]},
+        "id": {"type": ["string", "null"]}
     },
-    "error": {
-      "type": ["string", "null"]
-    },
-    "id": {
-      "type": ["string", "null"]
-    }
-  },
-  "required": [
-    "result",
-    "error",
-    "id"
-  ]
+    "required": ["result", "error", "id"]
 }
 
 schema_block_hash = {
-  "type": "object",
-  "properties": {
-    "result": {
-      "type": "string"
+    "type": "object",
+    "properties": {
+        "result": {"type": "string"},
+        "error": {"type": ["string", "null"]},
+        "id": {"type": ["string", "null"]}
     },
-    "error": {
-      "type": ["string", "null"]
-    },
-    "id": {
-      "type": ["string", "null"]
-    }
-  },
-  "required": [
-    "result",
-    "error",
-    "id"
-  ]
+    "required": ["result", "error", "id"]
 }
 
 schema_confirmed_raw_tx = {
-  "type": "object",
-  "properties": {
-    "result": {
-      "type": "object",
-      "properties": {
-        "txid": {
-          "type": "string"
+    "type": "object",
+    "properties": {
+        "result": {
+            "type": "object",
+            "properties": {
+                "txid": {"type": "string"},
+                "hash": {"type": "string"},
+                "version": {"type": "integer"},
+                "size": {"type": "integer"},
+                "vsize": {"type": "integer"},
+                "weight": {"type": "integer"},
+                "locktime": {"type": "integer"},
+                "vin": {
+                    "type": "array",
+                    "items": {"type": "object"}
+                },
+                "vout": {
+                    "type": "array",
+                    "items": {"type": "object"}
+                },
+                "hex": {"type": "string"},
+                "blockhash": {"type": "string"},
+                "confirmations": {"type": "integer"},
+                "time": {"type": "integer"},
+                "blocktime": {"type": "integer"}
+            },
+            "required": [
+                "txid",
+                "hash",
+                "version",
+                "size",
+                "vsize",
+                "weight",
+                "locktime",
+                "vin",
+                "vout",
+                "hex",
+                "blockhash",
+                "confirmations",
+                "time",
+                "blocktime"
+            ]
         },
-        "hash": {
-          "type": "string"
-        },
-        "version": {
-          "type": "integer"
-        },
-        "size": {
-          "type": "integer"
-        },
-        "vsize": {
-          "type": "integer"
-        },
-        "weight": {
-          "type": "integer"
-        },
-        "locktime": {
-          "type": "integer"
-        },
-        "vin": {
-          "type": "array",
-          "items": {
-            "type": "object"
-          }
-        },
-        "vout": {
-          "type": "array",
-          "items": {
-            "type": "object"
-          }
-        },
-        "hex": {
-          "type": "string"
-        },
-        "blockhash": {
-          "type": "string"
-        },
-        "confirmations": {
-          "type": "integer"
-        },
-        "time": {
-          "type": "integer"
-        },
-        "blocktime": {
-          "type": "integer"
-        }
-      },
-      "required": [
-        "txid",
-        "hash",
-        "version",
-        "size",
-        "vsize",
-        "weight",
-        "locktime",
-        "vin",
-        "vout",
-        "hex",
-        "blockhash",
-        "confirmations",
-        "time",
-        "blocktime"
-      ]
+        "error": {"type": ["string", "null"]},
+        "id": {"type": ["string", "null"]}
     },
-    "error": {
-      "type": [
-        "string",
-        "null"
-      ]
-    },
-    "id": {
-      "type": [
-        "string",
-        "null"
-      ]
-    }
-  },
-  "required": [
-    "result",
-    "error",
-    "id"
-  ]
+    "required": ["result", "error", "id"]
 }
 
 schema_general = {
-  "type": "object",
-  "properties": {
-    "result": {
-      "type": "string"
+    "type": "object",
+    "properties": {
+        "result": {"type": "string"},
+        "error": {"type": ["string", "null"]},
+        "id": {"type": ["string", "null"]}
     },
-    "error": {
-      "type": ["string", "null"]
-    },
-    "id": {
-      "type": ["string", "null"]
-    }
-  },
-  "required": [
-    "result",
-    "error",
-    "id"
-  ]
+    "required": ["result", "error", "id"]
 }
